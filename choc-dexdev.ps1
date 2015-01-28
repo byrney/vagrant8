389ea129544e7e00dec476c2d2784c837b03eb8d
Robert Byrne <robert@byrnemail.org>
2015-01-28 07:39:11 +0000

    Erlang is in programfiles not x86

--- choc-dexdev.ps1
+++ choc-dexdev.ps1
@@ -1,9 +1,4 @@
 
-$programfiles=${env:ProgramFiles(x86)}
-if($programfiles -eq "") {
-    $programfiles=${env:ProgramFiles}
-}
-
 write-host "Erlang"
 cinst erlang
-[Environment]::SetEnvironmentVariable("ERLANG_HOME", "c:\$programfiles\erl6.2", "Machine")
+[Environment]::SetEnvironmentVariable("ERLANG_HOME", "${env:ProgramFiles}\erl6.2", "Machine")
