b00fa416e1cd3b646bf7eb1ace55c45abe3da031
Nicola Gallo <nicolagallo@Nicolas-MBP.home>
2015-07-29 23:16:15 +0100

    Fix for Ruby certificate

--- choc-ruby.ps1
+++ choc-ruby.ps1
@@ -5,6 +5,6 @@ start-process -wait -verb runas cinst ruby2.devkit
 start-process -wait -verb runas cinst rubygems
 
 # workaround for missing certs in ruby prior to 2.4
-xcopy /d \vagrant\AddTrustExternalCARoot-2048.pem c:\tools\ruby215\lib\ruby\site_ruby\2.1.0\rubygems\ssl_certs
+echo f | xcopy \\VBOXSRV\vagrant\AddTrustExternalCARoot-2048.pem c:\tools\ruby215\lib\ruby\site_ruby\2.1.0\rubygems\ssl_certs
 gem source -a http://rubygems.org
 
