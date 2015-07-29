d40b187fa4c3c71dac6439a6ba0f16cd55552b90
Nicola Gallo <nicolagallo@Nicolas-MBP.home>
2015-07-29 22:52:00 +0100

    Amended vagrant folder path

--- choc-dotnetdev.ps1
+++ choc-dotnetdev.ps1
@@ -14,7 +14,8 @@ function make-link
 }
 
 write-host "Dot net dev tools"
-choco install -y visualstudiocommunity2013dex -source 'c:\vagrant'
+xcopy 
+choco install -y visualstudiocommunity2013dex -source '\\VBOXSRV\vagrant'
 cinst -y NuGet.CommandLine 
 make-link "c:\Users\Public\Desktop\VS2013" "$programfiles\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
 
