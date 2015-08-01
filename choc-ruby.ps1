b1d87dd20f3724e1d77a181ce00b8c9180524be0
Robert Byrne <robert@byrnemail.org>
2015-08-01 15:57:37 +0100

    Pin ruby versions using obscure PS syntax

--- choc-ruby.ps1
+++ choc-ruby.ps1
@@ -1,8 +1,8 @@
 
 
-start-process -wait -verb runas cinst ruby -version 2.1.6
+start-process -wait -verb runas -argumentlist "ruby -version 2.1.6" cinst
 start-process -wait -verb runas cinst ruby2.devkit
-start-process -wait -verb runas cinst rubygems -version 2.4.1.0
+start-process -wait -verb runas cinst -argumentlist "rubygems -version 2.4.1.0"
 
 # workaround for missing certs in ruby prior to 2.4
 echo f | xcopy /i /d \vagrant\AddTrustExternalCARoot-2048.pem c:\tools\ruby21\lib\ruby\site_ruby\2.1.0\rubygems\ssl_certs\
