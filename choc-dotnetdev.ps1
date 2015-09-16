c4ca7f1964cb477981d04d58cc37b6dad5af6245
Robert Byrne <robert@byrnemail.org>
2015-09-16 20:16:58 +0100

    vs2015

--- choc-dotnetdev.ps1
+++ choc-dotnetdev.ps1
@@ -1,5 +1,6 @@
 write-host "Dot net dev tools"
-choco install -y visualstudiocommunity2013dex -source '\\VBOXSRV\vagrant'
+# choco install -y visualstudiocommunity2013dex -source '\\VBOXSRV\vagrant'
+choco install visualstudio2015community
 cinst -y NuGet.CommandLine 
-make-link "c:\Users\Public\Desktop\VS2013" "$programfiles\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+make-link "c:\Users\Public\Desktop\VS2015" "$programfiles\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
 
